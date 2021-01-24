8bad1569737a2e19118a8b0d04d7e6cc64e05f3e
Rob Bos <raj.bos@gmail.com>
2021-01-24 17:20:30 +0100

    Adding lops to check all repos in an org

--- updater.ps1
+++ updater.ps1
@@ -1,15 +1,68 @@
-$repoUrl = "https://api.github.com/repos/rajbos-actions/test-repo"
+# example parameters:
+#$repoUrl = "https://api.github.com/repos/rajbos-actions/test-repo"
+#$orgName = "rajbos-actions"
 
+$CentralHeaders
+function Get-Headers {
+    param (        
+        [string] $userName,
+        [string] $PAT
+    )
+
+    if ($null -ne $CentralHeaders) {
+        return $CentralHeaders
+    }
+
+    $pair = "$($userName):$($PAT)"
+    $encodedCreds = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($pair))
+    $basicAuthValue = "Basic $encodedCreds"
+
+    $CentralHeaders = @{
+        Authorization = $basicAuthValue
+    }
+
+    return $CentralHeaders
+}
+
+function CallWebRequest {
+    param (
+        [string] $url,
+        [string] $userName,
+        [string] $PAT
+    )
+
+    $Headers = Get-Headers
+
+    $result = Invoke-WebRequest -Uri $url -Headers $Headers       
+    $info = ($result.Content | ConvertFrom-Json)
+
+    return $info
+
+}
+
+function FindAllRepos {
+    param (
+        [string] $orgName,
+        [string] $userName,
+        [string] $PAT
+    )
+
+    $url = "https://api.github.com/orgs/$orgName/repos"
+    $info = CallWebRequest -url $url -userName $userName -PAT $PAT
+
+    Write-Host "Found [$($info.Count)] repositories in [$orgName]"
+    return $info
+}
 
 function FindRepoOrigin {
     param (
-        [string] $repoUrl
+        [string] $repoUrl,
+        [string] $userName,
+        [string] $PAT
     )
 
-    $result = Invoke-WebRequest -Uri $repoUrl
+    $info = CallWebRequest -url $repoUrl -userName $userName -PAT $PAT
         
-    $info = ($result.Content | ConvertFrom-Json)
-
     if ($false -eq $info.fork) {
         Write-Error "The repo with url [$repoUrl] is not a fork"
         throw
@@ -26,10 +79,9 @@ function FindRepoOrigin {
         Write-Host "There are new updates on the parent available"
     }
 
-    # find the compare url
-    # https://github.com/rajbos-actions/test-repo/compare/main...rajbos:main
+    # build the compare url
     $compareUrl = "https://github.com/$($info.full_name)/compare/$defaultBranch..$($info.parent.owner.login):$defaultBranch"
-    Write-Host "You can compare the default branches using this link: $compareUrl"
+    Write-Host "You can compare the default branches using this link: [ $compareUrl ]"
 
     return @{
         parentUrl = $info.parent.html_url
@@ -37,14 +89,65 @@ function FindRepoOrigin {
         lastPushRepo = $info.pushed_at
         lastPushParent = $info.parent.pushed_at
         updateAvailable = ($info.pushed_at -lt $info.parent.pushed_at)
+        compareUrl = $compareUrl
     }
 }
 
 
-$parent = FindRepoOrigin -repoUrl $repoUrl
-if ($null -ne $parent) {
-    Write-Host "The repo is forked and the fork is from [$($parent.parentUrl)]"
-    return $parent.updateAvailable
+function GetParentHasUpdatesAvailable {
+    param (
+        [string] $repoUrl,
+        [string] $userName,
+        [string] $PAT
+    )
+
+    $parent = FindRepoOrigin -repoUrl $repoUrl -userName $userName -PAT $PAT
+    if ($null -ne $parent) {
+        Write-Host "The repo is forked and the fork is from [$($parent.parentUrl)]"
+        return $parent.updateAvailable
+    }
+
+    return $false
 }
 
-return $false
+
+function CheckAllReposInOrg {
+    param (
+        [string] $orgName,
+        [string] $userName,
+        [string] $PAT
+    )
+    $repos = FindAllRepos -orgName $orgName -userName $userName -PAT $PAT
+
+    # create hastable
+    $reposWithUpdates = @()
+
+    foreach ($repo in $repos) {
+        if ($repo.fork) {
+            Write-Host "Checking repository [$($repo.full_name)]"
+            $repoInfo = FindRepoOrigin -repoUrl $repo.url
+            if ($repoInfo.updateAvailable) {
+                Write-Host "Found new updates in the parent repository [$($repoInfo.parentUrl)], compare the changes with [$($repoInfo.compareUrl)]"
+
+                $repo = ${
+                    repoName = $repo.full_name
+                    parentUrl = $repoInfo.parentUrl
+                    compareUrl = $repoInfo.compareUrl
+                }
+
+                $reposWithUpdates += $repo
+            } 
+            else {
+                Write-Host "No updates available from parent"
+            }
+        }
+        else {
+            Write-Host "Skipping repository [$($repo.full_name)] since it is not a fork"
+        }
+    }
+
+    Write-Host "Found [$($reposWithUpdates.Count)] forks with available updates"
+}
+
+# example calls:
+# CheckAllReposInOrg -orgName "rajbos-actions" -userName "xxx" -PAT $env:GitHubPAT
