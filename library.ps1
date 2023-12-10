b7b9d0360d99c0c7e5db2b28fcc3e0310a9d9865
Rob Bos <rajbos@users.noreply.github.com>
2023-12-10 13:11:01 +0000

    Adding pagination support for user and org repos

--- library.ps1
+++ library.ps1
@@ -9,27 +9,30 @@ function FindAllRepos {
     )
 
     Write-Debug "Finding all repos with orgName: [$orgName], userName: [$userName], PAT lenght: [$($PAT.length)]"
-
+    $actionsLocation = $orgName
     if ($null -ne $orgName -And $orgName.Length -ne 0) { 
         Write-Debug "Finding all repos for org with name [$orgName]"      
         $url = "https://api.github.com/orgs/$orgName/repos?per_page=100"
         $info = CallWebRequest -url $url -userName $userName -PAT $PAT
 
+        Write-Debug "Found [$($info.Count)] repositories in [$actionsLocation]"
         Write-Debug "info.GetType: [$($info.GetType())]"
-        if ($info.GetType() -ne "System.Object[]")
+        if ($info.GetType().ToString() -ne "System.Object[]")
         {
+            $actionsLocation = $userName
             Write-Warning "Error loading information from org with name [$orgName], trying with user based repository list"
             $url = "https://api.github.com/users/$orgName/repos"
             $info = CallWebRequest -url $url -userName $userName -PAT $PAT
         }
     }
     else {
+        $actionsLocation = $userName
         Write-Debug "Finding all repos for user with name [$userName]"
         $url = "https://api.github.com/users/$userName/repos"
         $info = CallWebRequest -url $url -userName $userName -PAT $PAT
     }
 
-    Write-Host "Found [$($info.Count)] repositories in [$orgName]"
+    Write-Host "Found [$($info.Count)] repositories in [$actionsLocation]"
     Write-Debug "info.GetType: [$($info.GetType())]"
     return $info
 }
